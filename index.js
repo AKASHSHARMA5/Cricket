60279f0d35add3bcc3c176c38c016cbf30e3afd7
Akash sharma <131224911+AKASHSHARMA5@users.noreply.github.com>
2023-06-01 13:05:20 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,12 @@
+let x=function(e){
+    console.log("hello new player");
+    document.location.href='index2.html';
+}
+document.getElementById("newplayer").addEventListener("click",x);
+
+let y=function(e){
+    console.log("hello new player");
+    document.location.href='index.html';
+}
+document.getElementById("trending").addEventListener("click",y);
+//https://www.mykhel.com/img/2023/05/rinku-singh-ipl-2023-1685467805.jpg
